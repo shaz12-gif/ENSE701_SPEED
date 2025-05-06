3ab6781b2a638618d2fb3db75e2f2bbc4089f924
OO25 <130468221+OO25@users.noreply.github.com>
2025-05-06 20:05:29 +1200

    feat: update backend dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    MongooseModule.forRoot(
+      process.env.DB_URI || 'mongodb://localhost:27017/default-db',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
